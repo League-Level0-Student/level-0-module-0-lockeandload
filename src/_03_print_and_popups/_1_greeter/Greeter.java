2189099c6c6d74d23592bf1d1e7c73270b25a5cd
jereycui2008 <jeremycuid@gmail.com>
2023-05-04 19:56:12 -0700

    I'm not a banana

--- src/_03_print_and_popups/_1_greeter/Greeter.java
+++ src/_03_print_and_popups/_1_greeter/Greeter.java
@@ -1,7 +1,13 @@
 package _03_print_and_popups._1_greeter;
 
+import javax.swing.JOptionPane;
+
 public class Greeter {
 public static void main(String[] args) {
-	public class print {Hello World!}
+	System.out.println("Hello World");
+	String input = JOptionPane.showInputDialog("What is your name?");
+	JOptionPane.showMessageDialog(null,"Hello " +input);
+
 }
+
 }
--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,11 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String math = JOptionPane.showInputDialog("whats 1+1=?");
 		// 2. If they say "yes", tell them they will rule the world.
-
+if(math.equals("window")) {
+	JOptionPane.showMessageDialog(null,"Your a banana ");
+}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
